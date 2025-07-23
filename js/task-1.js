3dc431ac0b6d09621c492b289415224df2cc29ae
Ka-teryna <159587758+Ka-teryna@users.noreply.github.com>
2025-07-23 22:57:29 +0300

    first task

--- js/task-1.js
+++ js/task-1.js
@@ -1,29 +1,9 @@
-
-const customer = {
-  username: "Mango",
-  balance: 24000,
-  discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-};
-
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+const categoryList = document.querySelector('#categories');
+const categoryItems = categoryList.querySelectorAll('.item');
+console.log(`Number of categories: ${categoryItems.length}`);
+categoryItems.forEach(item => {
+    const categoryTitle = item.querySelector('h2').textContent;
+    const categoryElements = item.querySelectorAll('ul > li');
+    console.log(`Category: ${categoryTitle}`);
+    console.log(`Elements: ${categoryElements.length}`);
+    });
